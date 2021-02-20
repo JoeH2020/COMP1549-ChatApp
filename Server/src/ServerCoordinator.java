dff208b85456f352878acdb026c554ceceb4748e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-20 13:00:07 +0000

    Update ServerCoordinator.java
    Modified the class and made it functional. Needs testing.

--- Server/src/ServerCoordinator.java
+++ Server/src/ServerCoordinator.java
@@ -1,14 +1,70 @@
-public class ServerCoordinator extends Coordinator{
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
+public class ServerCoordinator extends Coordinator {
     public ServerCoordinator(ICommunicator communicator) {
         super(communicator);
     }
 
+    private final String PING_MESSAGE = "CHECKALIVE";
+    private final String PING_CONFIRM = "ALIVE";
+    private final int TIMEOUT_SECONDS = 15;
+
+    // Getting the coordinator's ip();
+    String targetIP = communicator.getTargetIP();
+    // Getting the coordinator's port()
+    String targetPort = communicator.getTargetPort();
+
+    Member coordinator;
+    Scanner in ;
+    PrintWriter out;
+
     @Override
     public void run() {
-        // getTargetIP()
-        // getTargetPort()
-        // (they'd be ClientCoordinator ip and port)
-        // connect to them
-        // check if he's alive
+        coordinator.setIP(targetIP);
+        coordinator.setPort(targetPort);
+        ServerSocket listener = new ServerSocket(59001);
+        Socket socket = listener.accept();
+
+        try {
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            out.println(PING_MESSAGE);
+            Instant time1 = Instant.now();
+
+            while (true) {
+                if ( in.hasNextLine() && in.nextLine().equals(PING_CONFIRM)) {
+                    //Coordinator is still alive
+                    break;
+                } else if (Duration.between(time1, Instant.now()).getSeconds() > TIMEOUT_SECONDS) {
+                    //Coordinator is not responding so we need a new one
+                    try {
+                        socket.close();
+                        Member[] members = communicator.getMembers();
+                        coordinator.setIP(members[0].getIP());
+                        coordinator.setPort(members[0].getPort());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        } finally {
+            if (!(listener.isClosed())) {
+                try {
+                    listener.close();
+                    socket.close();
+                } catch (IOException e )
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
