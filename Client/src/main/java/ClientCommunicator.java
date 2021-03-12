3c34f9bd74d18cdacfdbbc68bea3b3c00bfd81a5
Filippo De Angelis <filippodeangelis0@gmail.com>
2021-03-12 20:13:01 +0100

    client communicator no longer freezes waiting for input

--- Client/src/main/java/ClientCommunicator.java
+++ Client/src/main/java/ClientCommunicator.java
@@ -15,6 +15,9 @@ public class ClientCommunicator implements ICommunicator {
     public Scanner in;
     public PrintWriter out;
 
+    private InputQueue userInputQueue = new InputQueue(System.in);
+    private InputQueue serverInputQueue;
+
 
     public ClientCommunicator(String uniqueID, String selfIP, String selfPort, String serverIP, String serverPort) {
         this.self = new Member(uniqueID, selfIP, selfPort);
@@ -24,25 +27,37 @@ public class ClientCommunicator implements ICommunicator {
     }
 
     private void openSession() throws IOException {
+        userInputQueue.start();
         try {
             Socket socket = new Socket(serverIP, 59001);
-            in = new Scanner(socket.getInputStream());
+            serverInputQueue = new InputQueue(socket.getInputStream());
+            serverInputQueue.start();
             out = new PrintWriter(socket.getOutputStream(), true);
 
-            while (in.hasNextLine()) {
-                String line = in.nextLine();
-                if (line.startsWith("SUBMITNAME")) {
-//                  Need to call the method to print out the username here
-                    out.println("SYSTEM: CALL METHOD TO PRINT NAME");
-                } else if (line.startsWith("NAMEACCEPTED")) {
-//                  Need to call the method to update the GUI window with the username and allow user entry
-                    out.println("SYSTEM: CALL METHOD TO UPDATE USERNAME");
-//                    this.frame.setTitle("Chatter - " + line.substring(13));
-//                    textField.setEditable(true);
-                } else if (line.startsWith("MESSAGE")) {
-//                  Need to call the method to add the user message to the active window
-                    out.println("SYSTEM: CALL METHOD TO APPEND INCOMING MESSAGE");
-//                    messageArea.append(line.substring(8) + "\n");
+            while (true) {
+                if(serverInputQueue.hasItems()) {
+                    String line = serverInputQueue.nextItem();
+                    if (line.startsWith("SUBMITNAME")) {
+    //                  Need to call the method to print out the username here
+                        out.println(self.getUID());
+                    } else if (line.startsWith("NAMEACCEPTED")) {
+    //                  Need to call the method to update the GUI window with the username and allow user entry
+                        out.println("SYSTEM: CALL METHOD TO UPDATE USERNAME");
+    //                    this.frame.setTitle("Chatter - " + line.substring(13));
+    //                    textField.setEditable(true);
+                    } else if (line.startsWith("MESSAGE")) {
+    //                  Need to call the method to add the user message to the active window
+                        out.println("SYSTEM: CALL METHOD TO APPEND INCOMING MESSAGE");
+    //                    messageArea.append(line.substring(8) + "\n");
+                    } else {
+                        System.out.println(line);
+                    }
+                }
+
+                if (userInputQueue.hasItems()) {
+                    String item = userInputQueue.nextItem();
+                    System.out.println("Sent message: " + item);
+                    out.println(item);
                 }
             }
         } finally {
@@ -69,25 +84,25 @@ public class ClientCommunicator implements ICommunicator {
     }
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.err.println("You need only to pass the server IP in as an argument.");
+        if (args.length != 2) {
+            System.err.println("Please specify name and IP.");
             return;
         }
 
 
-        int membersSize = members.size(); //To do this I made members static, change if this is a problem.
-        membersSize++;
-        String uniqueID = String.valueOf(membersSize); //Used to create a unique ID, check length and add 1
-        String serverIP = args[0];
+//        int membersSize = members.size(); //To do this I made members static, change if this is a problem.
+//        membersSize++;
+//        String uniqueID = String.valueOf(membersSize); //Used to create a unique ID, check length and add 1
+        String serverIP = args[1];
+//
+//        Socket socket = new Socket(args[0], 59001);
+//
+//        String selfIP = String.valueOf(socket.getInetAddress()); //Getting the IP of the Client as a String
+//        String selfPort = String.valueOf(socket.getPort()); //Getting the port number of client as a String
 
-        Socket socket = new Socket(args[0], 59001);
-
-        String selfIP = String.valueOf(socket.getInetAddress()); //Getting the IP of the Client as a String
-        String selfPort = String.valueOf(socket.getPort()); //Getting the port number of client as a String
 
 
-
-        ClientCommunicator client = new ClientCommunicator(uniqueID, selfIP ,selfPort , serverIP, serverPort);
+        ClientCommunicator client = new ClientCommunicator(args[0], "127.0.0.1", "59001", serverIP, serverPort);
 
         client.openSession(); //Start the session
     }
---
+++ Client/src/main/java/InputQueue.java
@@ -0,0 +1,40 @@
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// This class has the purpose of reading and storing user input, so that callin Scanner.nextLine() doesn't freeze
+// the whole program
+public class InputQueue {
+    LinkedList<String> inputQueue = new LinkedList<>();
+    Thread t;
+
+    public InputQueue(InputStream inputStream) {
+        t = new Thread() {
+            public void run() {
+                Scanner in = new Scanner(inputStream);
+                while(true) {
+                    if (in.hasNextLine())
+                        inputQueue.add(in.nextLine());
+                }
+            }
+        };
+    }
+
+    public boolean hasItems() {
+        return !inputQueue.isEmpty();
+    }
+
+    public String nextItem() {
+        return inputQueue.pop();
+    }
+
+    public void start() {
+        t.start();
+    }
+
+    public void stop() {
+        t.interrupt();
+    }
+
+}
