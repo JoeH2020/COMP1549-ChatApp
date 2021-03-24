3ded39dc3a24c3ba29f58c574fede4ad3d0b8332
Filippo De Angelis <filippodeangelis0@gmail.com>
2021-03-24 13:10:09 +0100

    added viewMembers test

--- Server/src/main/test/ServerThreadTest.java
+++ Server/src/main/test/ServerThreadTest.java
@@ -229,6 +229,56 @@ public class ServerThreadTest {
         Mockito.verify(singleton, Mockito.times(1)).broadcast("MESSAGE1500Hello World");
     }
 
+    @Test
+    public void viewMembers() throws NoSuchFieldException, IllegalAccessException, IOException, InterruptedException {
+        // this test makes sure a correct message is sent out when requesting online members
+
+        // let's make a list of members that we want to be returned
+        HashSet<Member> members = new HashSet<>();
+        members.add(new Member("John", null, null));
+        members.add(new Member("George", null, null));
+
+        // we need to mock the singleton for this
+        ServerSingleton singleton = Mockito.mock(ServerSingleton.class);
+        when(singleton.getMembers()).thenReturn(members);
+        when(singleton.getTime()).thenReturn("1500");
+
+        // now we need to make sure the actual singleton class returns this instance when asked
+        // there is no way other than reflections to do this (without PowerMockito)
+        Class singletonClass = ServerSingleton.class;
+        Field instanceField = singletonClass.getDeclaredField("instance");
+        instanceField.setAccessible(true);
+        instanceField.set(null, singleton);
+
+        // mock the socket so that it returns our OutputStream when requested
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Socket socket = Mockito.mock(Socket.class);
+        when(socket.getOutputStream()).thenReturn(outputStream);
+
+        // now create an appropriate InputStream for the class to work properly
+        // the class requests the name
+        String input = "Paul\nVIEWMEMBERS\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        when(socket.getInputStream()).thenReturn(inputStream);
+
+        // also mock getInetAddress and getPort for everything to work
+        when(socket.getInetAddress()).thenReturn(InetAddress.getLocalHost());
+        when(socket.getPort()).thenReturn(7738);
+
+        // now run the class
+        ServerThread thread = new ServerThread(socket);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(thread);
+
+        // wait a bit for everything to work
+        Thread.sleep(1000);
+
+        // make sure the outputStream has been written what we expect
+        String expectedString = "SUBMITNAME\nNAMEACCEPTED\nMEMBERSPaul,George,John";
+        String outputString = outputStream.toString("UTF-8");
+
+    }
+
     /*
     TODO
     test all keywords:
